e3d8212b152d8c03c59c2831cff4d5a436fc4af0
Sam Gunner <samgunnerpc@live.com>
2016-04-12 19:16:52 +0100

    made it work.

--- assets/js/dirList.js
+++ assets/js/dirList.js
@@ -1,10 +1,12 @@
 /*
     Directory-listing file for file browser. Interacts with angularjs elements and php at /int.php
 */
-var currentPath;
 
 var app = angular.module("mainApp", []);
 app.controller("mainCont", function($scope, $http) {
+    $scope.currentPath = "/";
+    $scope.files = getContents($scope.currentPath);
+    
     function getContents(filepath) {
         var address = "/int.php?dir=" + filepath;
         console.log(address);
@@ -18,7 +20,28 @@ app.controller("mainCont", function($scope, $http) {
         return response;
     }
     
-    $scope.currentPath = "/";
-    currentPath = $scope.currentPath;
-    $scope.files = getContents($scope.currentPath);
+    $scope.changeDir = function(dir) {
+        var changetodir;
+        if (dir == "..") {
+            var tbl = $scope.currentPath.split("/");
+            tbl.pop();
+            changetodir = tbl.join("/");
+        } else if (dir.length > 0) {
+            changetodir = $scope.currentPath + "/" + dir;
+        } else {
+            changetodir = $scope.currentPath;
+        }
+        
+        if (changetodir.substr(0, 2) === "//") {
+            changetodir = changetodir.substr(1, changetodir.length - 1);
+        }
+        
+        $scope.currentPath = changetodir;
+        $scope.files = getContents($scope.currentPath);
+    }
+    
+    $scope.getFullFilePath = function(filename) {
+        var path = $scope.currentPath + "/" + filename;
+        return path;
+    }
 });
